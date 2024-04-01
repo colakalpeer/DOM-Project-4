a3c0c9ac9f5ac38e2d278f1ef0691163df60b0f0
colakalpeer <colakalpeer93@gmail.com>
2024-04-01 09:02:42 +0300

    fixed: JavaScript codes about timer

--- project-4.js
+++ project-4.js
@@ -15,6 +15,12 @@ let leadingSeconds = 0;
 let leadingMinutes = 0;
 let leadingHours = 0;
 
+// Variables for set interval & timerstatus
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+
 // Stop Watch Function
 
 function stopWatch() {
@@ -55,4 +61,29 @@ function stopWatch() {
     leadingHous + ":" + leadingMinutes + ":" + leadingSeconds;
 }
 
-window.setInterval(stopWatch, 1)
+
+
+
+startStopBtn.addEventListener('click', function(){
+    if(timerStatus === "stopped") {
+        timerInterval = window.setInterval(stopWatch, 1000);
+        document.getElementById('startStopBtn').innerHTML = `<i 
+        class="fa-solid fa-pause" id="pause"></i>`;
+        timerStatus = "started";
+    } else {
+        window.clearInterval(timerInterval);
+        document.getElementById('startStopBtn').innerHTML = `<i
+        class="fa-solid fa-play" id="play"></i>`;
+        timerStatus = "stopped";
+    }
+});
+
+resetBtn.addEventListener('click', function(){
+
+    window.clearInterval(timerInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    document.getElementById('timer').innerHTML = "00:00:00";
+})
