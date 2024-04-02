0653aa40f89b917f5b3209d40b53e25214a69aba
colakalpeer <colakalpeer93@gmail.com>
2024-04-02 08:20:26 +0300

    ui: Js improvement made

--- project-4.js
+++ project-4.js
@@ -1,7 +1,5 @@
-// Variables for buttons
-
-const startStopBtn = document.querySelector('#startStopBtn');
-const resetBtn = document.querySelector('#resetBtn');
+const startStopBtn = document.querySelector("#startStopBtn");
+const resetBtn = document.querySelector("#resetBtn");
 
 // Variables for time values
 
@@ -15,75 +13,69 @@ let leadingSeconds = 0;
 let leadingMinutes = 0;
 let leadingHours = 0;
 
-// Variables for set interval & timerstatus
+// Vairables for set interval & timerstatus
 
 let timerInterval = null;
 let timerStatus = "stopped";
 
-
 // Stop Watch Function
 
 function stopWatch() {
+  seconds++;
 
-    seconds++
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
 
-    if(seconds / 60 === 1){
-        secconds = 0;
-        minutes ++;
-
-        if(minutes / 60 ===1) {
-            minutes = 0;
-            hours++
-        }
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours++;
     }
+  }
 
+  if (seconds < 10) {
+    leadingSeconds = "0" + seconds.toString();
+  } else {
+    leadingSeconds = seconds;
+  }
+  if (minutes < 10) {
+    leadingMinutes = "0" + minutes.toString();
+  } else {
+    leadingMinutes = minutes;
+  }
+  if (hours < 10) {
+    leadingHours = "0" + hours.toString();
+  } else {
+    leadingHours = hours;
+  }
 
-    if(seconds < 10) {
-        leadingSeconds = "0" + seconds.toString();
-    
-    } else {
-        leadingSeconds = seconds;
-    }
-
-    if(minutes < 10) {
-        leadingMinutes = "0" + minutes.toString();
-    } else {
-        leadingMinutes = minutes;
-    }
-
-    if(hours < 10) {
-        leadingHours = "0" + hours.toString();
-    } else {
-        leadingHours = hours;
-    }
-
-    let displayTimer = document.getElementById('timer').innerText =
-    leadingHous + ":" + leadingMinutes + ":" + leadingSeconds;
+  let displayTimer = (document.getElementById("timer").innerText =
+    leadingHours + ":" + leadingMinutes + ":" + leadingSeconds);
 }
 
+//
 
-
-
-startStopBtn.addEventListener('click', function(){
-    if(timerStatus === "stopped") {
-        timerInterval = window.setInterval(stopWatch, 1000);
-        document.getElementById('startStopBtn').innerHTML = `<i 
-        class="fa-solid fa-pause" id="pause"></i>`;
-        timerStatus = "started";
-    } else {
-        window.clearInterval(timerInterval);
-        document.getElementById('startStopBtn').innerHTML = `<i
-        class="fa-solid fa-play" id="play"></i>`;
-        timerStatus = "stopped";
-    }
+startStopBtn.addEventListener("click", function () {
+  if (timerStatus === "stopped") {
+    timerInterval = window.setInterval(stopWatch, 1000);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<i class="fa-solid fa-pause" id="pause"></i>`;
+    timerStatus = "started";
+  } else {
+    window.clearInterval(timerInterval);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<i class="fa-solid fa-play" id="play"></i>`;
+    timerStatus = "stopped";
+  }
 });
 
-resetBtn.addEventListener('click', function(){
+resetBtn.addEventListener("click", function () {
+  window.clearInterval(timerInterval);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
 
-    window.clearInterval(timerInterval);
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-
-    document.getElementById('timer').innerHTML = "00:00:00";
-})
+  document.getElementById("timer").innerHTML = "00:00:00";
+});
